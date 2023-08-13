b1cc6804fb5f6a2f02c5eafd0752a9671ee5f4ee
Roman Zavalishin <roman.zavalishin@webcom.cz>
2023-08-13 23:11:00 +0200

    Added delete book feature

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -2,12 +2,16 @@ using BlazorApp;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+
 builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
