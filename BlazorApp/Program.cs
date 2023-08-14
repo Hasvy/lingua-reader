62397c9bf6f16e7e63e44089b10ce4cbdfa0366d
Roman Zavalishin <roman.zavalishin@webcom.cz>
2023-08-14 11:55:23 +0200

    Added loading process

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -3,6 +3,7 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
+using Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,6 +12,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<DialogService>();
 builder.Services.AddScoped<NotificationService>();
+
+builder.Services.AddSingleton<ProgressService>();
 
 builder.Services.AddBlazoredLocalStorage();
 
