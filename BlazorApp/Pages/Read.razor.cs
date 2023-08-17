e017f8a464d8b9153370b2a8ef645e03bce476ce
Roman Zavalishin <roman.zavalishin@webcom.cz>
2023-08-17 11:18:57 +0200

    Added arrows to switch pages and switching pages feature

--- BlazorApp/Pages/Read.razor.cs
+++ BlazorApp/Pages/Read.razor.cs
@@ -11,26 +11,53 @@ namespace BlazorApp.Pages
 
         [Parameter]
         public string? BookId { get; set; }
-
         private List<string> Text { get; set; } = new List<string>();
+        private string _actualPage = null!;
+        private int _actualPageNumber;
 
         protected override async Task OnInitializedAsync()
         {
             //Get Book form localStorage
             var stringCover = await localStorage.GetItemAsync<string>(BookId);
             var cover = JsonConvert.DeserializeObject<BookCover>(stringCover);
+
             if (cover != null)
             {
                 var stringText = await localStorage.GetItemAsync<string>(cover.TextId.ToString());
                 Text = JsonConvert.DeserializeObject<List<string>>(stringText);
             }
 
-            //if (cover != null)
-            //{
-            //   Text = await localStorage.GetItemAsync<string>(cover.TextId.ToString());
-            //}
+            if (Text != null)
+            {
+                _actualPage = Text.First();
+            }
+
+            _actualPageNumber = GetIndexOfActualPage() + 1;
 
             await base.OnInitializedAsync();
+        }
+
+        private int GetIndexOfActualPage()
+        {
+            return Text.IndexOf(_actualPage);
+        }
+
+        private void NextPage()
+        {
+            if (_actualPageNumber != Text.Count)
+            {
+                _actualPage = Text[GetIndexOfActualPage() + 1];
+                _actualPageNumber++;
+            }
+        }
+
+        private void PreviousPage()
+        {
+            if (_actualPageNumber != 1)
+            {
+                _actualPage = Text[GetIndexOfActualPage() - 1];
+                _actualPageNumber--;
+            }
         }
     }
 }
