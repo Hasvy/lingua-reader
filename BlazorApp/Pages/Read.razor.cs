7ae0c78e4271f8d27bf77a812980586d7a87e571
Roman Zavalishin <roman.zavalishin@webcom.cz>
2023-10-05 00:48:52 +0200

    Cleared Read.razor.cs Added book loading

--- BlazorApp/Pages/Read.razor.cs
+++ BlazorApp/Pages/Read.razor.cs
@@ -1,30 +1,22 @@
-﻿using AngleSharp;
-using AngleSharp.Css.Dom;
-using AngleSharp.Dom;
-using AngleSharp.Html.Dom;
-using AngleSharp.Html.Parser;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using Objects.Entities;
 using Services;
-using System.Diagnostics;
+using System.Reflection.Metadata;
 
 namespace BlazorApp.Pages
 {
     public partial class Read : ComponentBase
     {
-        [Inject] HtmlParser htmlParser { get; set; } = null!;
-        [Inject] HtmlParserService htmlParserService { get; set; } = null!;
-        [Inject] IJSRuntime JS { get; set; } = null!;
+        //[Inject] HtmlParser htmlParser { get; set; } = null!;
+        //[Inject] HtmlParserService htmlParserService { get; set; } = null!;
         //[Inject] ILocalStorageService localStorageService { get; set; } = null!;
+        [Inject] IJSRuntime JS { get; set; } = null!;
         [Inject] BookOperationsService BookOperationsService { get; set; } = null!;
 
         [Parameter]
-        public string BookId { get; set; }
-
+        public string BookId { get; set; } = null!;
         private IList<BookSection> Sections { get; set; } = new List<BookSection>();
-        //private IEnumerable<BookContent> Content { get; set; } = new List<BookContent>();
-        private int maxHeight = 560;
         private int _actualPageNumber = 1;
         private int pagesCount = 0;
         private int actualSectionPagesCount = 0;
@@ -54,56 +46,25 @@ namespace BlazorApp.Pages
             currentSectionNumber = 0;
             actualSectionPagesCount = await JS.InvokeAsync<int>("divideAndSetHtml", Sections.First().Text);
 
+            await JS.InvokeVoidAsync("showContent");
             _isLoading = false;
             await base.OnInitializedAsync();
             //stopwatch.Stop();
             //Console.WriteLine("Time: " + stopwatch.ElapsedMilliseconds + " msec");
         }
 
-        private async Task DivideAndSetHtml(string content)
+        private async void ChangePage(int? pageNumber)
         {
-            //Getting iframeDocument from webpage
-            var context = BrowsingContext.New(Configuration.Default);
-            var iframeDocumentHtml = await JS.InvokeAsync<string>("getIframeDocument");
-            var document = await context.OpenAsync(req => req.Content(iframeDocumentHtml));
-
-            //Parse html of book section (content)
-            var parser = new HtmlParser();
-            var doc = await parser.ParseDocumentAsync(content);
-            if (document.Head is not null && doc.Head is not null)
+            foreach (var section in Sections)
             {
-                document.Head.InnerHtml = doc.Head.InnerHtml;
-                document.Body.InnerHtml = doc.Body.InnerHtml;
+                if (section.FirstPage <= pageNumber && pageNumber <= section.LastPage)
+                {
+                    _actualPageNumber = (int)pageNumber;
+                    currentSectionNumber = section.OrderNumber;
+                    actualSectionPagesCount = await JS.InvokeAsync<int>("divideAndSetHtml", section.Text);
+                    await JS.InvokeVoidAsync("setActualPage", _actualPageNumber - section.FirstPage);
+                }
             }
-
-            //Add class to book section to hide scrollbar
-            IHtmlLinkElement link = document.CreateElement<IHtmlLinkElement>();
-            link.Href = "http://localhost:5284/css/container.css";
-            link.Relation = "stylesheet";
-            link.Type = "text/css";
-            document.Head.AppendChild(link);
-
-            //Set iframeDocument with JS and return its offsetHeight
-            string html = document.ToHtml();
-            var iframeBodyOffsetHeight = await JS.InvokeAsync<int>("setIframeDocument", html);
-
-            //Dividing html on columns (pages) and setting additional style parameters
-            actualSectionPagesCount = SeparateHtmlToPages(iframeBodyOffsetHeight);
-            document.Body.SetStyle($"padding: 10; margin: 0; width: {900 * actualSectionPagesCount}; height: {maxHeight}; column-count: {actualSectionPagesCount}");
-            html = document.ToHtml();
-            await JS.InvokeAsync<int>("setIframeDocument", html);
-            //return actualSectionPagesCount;
-        }
-
-        private int SeparateHtmlToPages(int offsetHeight)
-        {
-            return (int)Math.Ceiling((double)offsetHeight / maxHeight);
-        }
-
-        private async void ChangePage(int pageNumber)
-        {
-            _actualPageNumber = pageNumber;
-            //await JS.InvokeVoidAsync("setActualPage", _actualPageNumber);
         }
 
         private async void NextPage()
@@ -144,10 +105,7 @@ namespace BlazorApp.Pages
             if (currentSectionNumber < Sections.Count())
             {
                 currentSectionNumber++;
-                //Stopwatch stopwatch = Stopwatch.StartNew();
                 await JS.InvokeAsync<int>("divideAndSetHtml", Sections[currentSectionNumber].Text);
-                //stopwatch.Stop();
-                //Console.WriteLine(stopwatch.ElapsedMilliseconds);
             }
         }
 
@@ -159,31 +117,5 @@ namespace BlazorApp.Pages
                 await JS.InvokeAsync<int>("divideAndSetHtml", Sections[currentSectionNumber].Text);
             }
         }
-
-        //For pdf       //For pdf maybe I can use freespire.pdf
-
-        //private int GetIndexOfActualPage()
-        //{
-        //    return Text.IndexOf(_actualPage);
-        //}
-
-        //private async void NextPage()
-        //{
-        //    await SetActualPageText();
-        //    if (_actualPageNumber != Text.Count)
-        //    {
-        //        _actualPage = Text[GetIndexOfActualPage() + 1];
-        //        _actualPageNumber++;
-        //    }
-        //}
-
-        //private void PreviousPage()
-        //{
-        //    if (_actualPageNumber != 1)
-        //    {
-        //        _actualPage = Text[GetIndexOfActualPage() - 1];
-        //        _actualPageNumber--;
-        //    }
-        //}
     }
 }
