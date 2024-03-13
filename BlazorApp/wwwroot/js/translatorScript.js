64e9f1d9dbc51756ce4da9264ddd713f6d51cd22
Roman Zavalishin <roman.zavalishin@webcom.cz>
2024-03-13 17:45:58 +0100

    Fix firefox getSelection text

--- BlazorApp/wwwroot/js/translatorScript.js
+++ BlazorApp/wwwroot/js/translatorScript.js
@@ -12,12 +12,13 @@ function getSelectedWord(hostElement) {
         }
 
         var wordRegexp = lang_regexp;
-        var s = hostElement.shadowRoot.getSelection();
-        var node = s.anchorNode;
-        if (node.nodeName != '#text') {            //Mb node.nodeName !== '#text'
+        //var selection = hostElement.shadowRoot.getSelection();
+        var selection = getSelectionText(hostElement);
+        var node = selection.anchorNode;
+        if (node.nodeName !== '#text') {
             return;
         }
-        var range = s.getRangeAt(0);
+        var range = selection.getRangeAt(0);
 
         // Finds a start point of a clicked word
         while ((range.startOffset > 0) && range.toString().match(wordRegexp)) {
@@ -66,6 +67,19 @@ function removeSpan() {
     if (span.parentNode != null) {
         span.parentNode.replaceChild(cont, span);
     }
+}
+
+function getSelectionText(hostElement) {
+    var selection;
+    try {
+        selection = hostElement.shadowRoot.getSelection();  //For Chrome and others
+    } catch (e) {
+        selection = document.getSelection();                //For Firefox
+    }
+    if (!selection || !selection.rangeCount) {
+        return;
+    }
+    return selection;
 }
 
 function speakWord(word, language) {
